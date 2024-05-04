5f1f354663a343764eef1174f8f92382b7044103
Tesche2 <nikolastesche@gmail.com>
2024-05-04 14:44:35 -0300

    ast files and parser with stack

--- include/ast.h
+++ include/ast.h
@@ -1,5 +1,5 @@
 // /include/ast.h
-// Lucas Kruger e Nicolas Tesche
+// Lucas Kruger e Nikolas Tesche
 
 #ifndef AST_H
 #define AST_H
@@ -20,6 +20,11 @@ typedef struct asl_node_type {
   struct asl_node_type *parent;
 } node_t;
 
+typedef struct node_stack {
+  node_t *node_data;
+  struct node_stack *prev_node;
+} node_s;
+
 extern void exporta(void *arvore);
 
 lex_val_t *create_lex_val(int line, tk_t type, char *value);
@@ -29,5 +34,8 @@ void add_child(node_t *par, node_t *child);
 void free_node(node_t *node);
 void print_tree(node_t *node);
 void print_tree(node_t *node);
+node_t *pop(node_s **node_stack);
+void push(node_s **node_stack, node_t *new_node);
+void print_stack(node_s *head);
 
 #endif
